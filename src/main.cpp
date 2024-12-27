7cd21013c4e895c9f10b15b614e9be46b2b80b3f
Loris Guerra <git.dehre@gmail.com>
2024-12-27 11:02:57 +0100

    rework interaction with fpga - new abstractions and commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,118 +1,152 @@
 #include <SPI.h>
 
-#define SPI_SPEED 500000 // 500kHz
-#define SPI_CLK_PIN 13
-#define SPI_MISO_PIN 12
-#define SPI_MOSI_PIN 11
-#define SPI_CS_PIN 10
-#define FPGA_RESET_PIN 9
+#define SPI_SPEED 5000 // 5kHz
 
-#define CMD_STATUS 0xFA
-#define CMD_READ   0xFB
-#define CMD_WRITE  0xFC
+// Arduino I/O Pins
+#define SCLK_PIN 13
+#define MISO_PIN 12
+#define MOSI_PIN 11
+#define CS_PIN 10
+#define RST_PIN 9
 
-#define ACK        0xAA
-#define NACK       0xBB
-#define EMPTY_BYTE 0xFE
-#define FULL_BYTE  0xFF
+// FPGA Commands
+#define CMD_COUNT 0xF0
+#define CMD_WRITE 0xF1
+#define CMD_READ 0xF2
+
+// FPGA Responses
+#define ACK 0xFA
+#define NACK 0xFB
+#define FIFO_EMPTY 0xFE
+#define FIFO_FULL 0xFF
 
 void begin_spi()
 {
     SPI.beginTransaction(SPISettings(SPI_SPEED, MSBFIRST, SPI_MODE0));
-    digitalWrite(SPI_CS_PIN, LOW);
+    digitalWrite(CS_PIN, LOW);
 }
 
 void end_spi()
 {
-    digitalWrite(SPI_CS_PIN, HIGH);
+    digitalWrite(CS_PIN, HIGH);
     SPI.endTransaction();
 }
 
-void test_status()
+void write_spi(uint8_t data[], size_t data_len)
 {
     begin_spi();
-    SPI.transfer(CMD_STATUS);
+    SPI.transfer(CMD_WRITE);
     delay(10);
-    for (int i = 10; i <= 50; i += 10)
+    uint8_t writing_response = SPI.transfer(0);
+    Serial.print("Writing response: 0x");
+    Serial.println(writing_response, HEX);
+    delay(10);
+    for (size_t i = 0; i < data_len; i++)
+    {
+        uint8_t bit_read = SPI.transfer(data[i]);
+        Serial.print("Writing: ");
+        Serial.print(data[i], DEC);
+        Serial.print("; Response: 0x");
+        Serial.println(bit_read, HEX);
+        delay(10); // 10ms
+    }
+    end_spi();
+}
+
+void read_spi(size_t len)
+{
+    begin_spi();
+    SPI.transfer(CMD_READ);
+    delay(10);
+    uint8_t reading_response = SPI.transfer(0);
+    Serial.print("Reading response: 0x");
+    Serial.println(reading_response, HEX);
+    delay(10);
+    for (size_t i = 0; i < len; i++)
     {
         uint8_t bit_read = SPI.transfer(0);
-        Serial.print("Bit read: ");
+        Serial.print("Read: ");
         Serial.println(bit_read, DEC);
         delay(10); // 10ms
     }
     end_spi();
-
-    delay(5000); // 5s
 }
 
-void test_set_get()
+void count_spi()
 {
-    {
-        begin_spi();
-        SPI.transfer(CMD_WRITE);
-        delay(10);
-        uint8_t initial_bit_read = SPI.transfer(0);
-        Serial.print("Initial writing: 0x");
-        Serial.println(initial_bit_read, HEX);
-        delay(10);
-        for (int i = 10; i <= 40; i += 10)
-        {
-            uint8_t bit_read = SPI.transfer(i);
-            Serial.print("Writing: ");
-            Serial.println(bit_read, DEC);
-            delay(10); // 10ms
-        }
-        end_spi();
-    }
+    begin_spi();
+    SPI.transfer(CMD_COUNT);
+    delay(10);
+    uint8_t count_response = SPI.transfer(0);
+    Serial.print("Count response: 0x");
+    Serial.println(count_response, HEX);
+    delay(10);
 
-    delay(1000);
+    uint8_t bit_read = SPI.transfer(0);
+    Serial.print("Count: ");
+    Serial.println(bit_read, DEC);
+    delay(10);
+    end_spi();
+}
 
-    {
-        begin_spi();
-        SPI.transfer(CMD_READ);
-        delay(10);
-        uint8_t initial_bit_read = SPI.transfer(0);
-        Serial.print("Initial reading: 0x");
-        Serial.println(initial_bit_read, HEX);
-        delay(10);
-        for (int i = 10; i <= 40; i += 10)
-        {
-            uint8_t bit_read = SPI.transfer(0);
-            Serial.print("Reading: ");
-            Serial.println(bit_read, DEC);
-            delay(10); // 10ms
-        }
-        end_spi();
-    }
-
-    delay(5000); // 5s
+// Set RST_PIN, then pulse SCLK_PIN
+void reset_spi()
+{
+    digitalWrite(SCLK_PIN, LOW);
+    delay(1); /* enough for any SPI_SPEED greater than 1kHz */
+    digitalWrite(RST_PIN, HIGH);
+    digitalWrite(SCLK_PIN, HIGH);
+    delay(1); /* enough for any SPI_SPEED greater than 1kHz */
+    digitalWrite(SCLK_PIN, LOW);
+    digitalWrite(RST_PIN, LOW);
 }
 
 void setup()
 {
-    pinMode(FPGA_RESET_PIN, OUTPUT);
-    pinMode(SPI_CS_PIN, OUTPUT);
-    pinMode(SPI_CLK_PIN, OUTPUT);
-    digitalWrite(FPGA_RESET_PIN, LOW);
-    digitalWrite(SPI_CS_PIN, HIGH);
-    digitalWrite(SPI_CLK_PIN, LOW);
+    // Initialize Arduino pins
+    pinMode(RST_PIN, OUTPUT);
+    pinMode(CS_PIN, OUTPUT);
+    pinMode(SCLK_PIN, OUTPUT);
+    digitalWrite(CS_PIN, HIGH);
 
-    // Initial delay to ensure both devices are connected
+    // Wait until both Arduino and FPGA are plugged in, then reset the FPGA
     delay(5000);
+    reset_spi();
 
-    // Reset the FPGA
-    digitalWrite(FPGA_RESET_PIN, HIGH);
-    digitalWrite(SPI_CLK_PIN, HIGH);
-    delay(1); /* enough for any SPI_SPEED greater than 1kHz */
-    digitalWrite(SPI_CLK_PIN, LOW);
-    digitalWrite(FPGA_RESET_PIN, LOW);
-
+    // Initialize Arduino peripherals
     Serial.begin(9600);
     SPI.begin();
-    delay(1000);
 }
 
 void loop()
 {
-    test_set_get();
+    Serial.println("WRITING ALL, READING ALL!");
+    uint8_t data_full_1[] = {10, 20, 30, 40, 50, 60, 70};
+    write_spi(data_full_1, sizeof(data_full_1));
+    delay(1000);
+    read_spi(sizeof(data_full_1));
+    delay(1000);
+
+    Serial.println("WRITING  4, READING 3, WRITING 2, READING ALL!");
+    uint8_t data_partial_1[] = {3, 6, 9, 12};
+    write_spi(data_partial_1, sizeof(data_partial_1));
+    delay(1000);
+    read_spi(3);
+    delay(1000);
+    uint8_t data_partial_2[] = {73, 76};
+    write_spi(data_partial_2, sizeof(data_partial_2));
+    delay(1000);
+    read_spi(7);
+    delay(1000);
+
+    Serial.println("WRITING ALL, READING 1, READING ALL!");
+    uint8_t data_full_2[] = {7, 14, 21, 28, 35, 42};
+    write_spi(data_full_2, sizeof(data_full_2));
+    delay(1000);
+    read_spi(1);
+    delay(1000);
+    read_spi(5);
+    delay(1000);
+
+    delay(10000); // 10s
 }
